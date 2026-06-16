217979b0569c396c71655dc3a4193e1dbeced945
azanaslam <140745739+azanaslam@users.noreply.github.com>
2026-06-16 19:05:44 +0500

    Update product queries to filter active products only

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,31 +1,61 @@
 const Product = require("../models/Product");
 
+
+
 const getProducts = async (_, res) => {
-  const products = await Product.find({ active: { $ne: false } }).sort({ createdAt: -1 });
+
+  const products = await Product.find({ active: true }).sort({ createdAt: -1 });
+
   res.json(products);
+
 };
+
+
 
 const getProductById = async (req, res) => {
-  const product = await Product.findOne({ _id: req.params.id, active: { $ne: false } });
+
+  const product = await Product.findOne({ _id: req.params.id, active: true });
+
   if (!product) return res.status(404).json({ message: "Product not found" });
+
   return res.json(product);
+
 };
+
+
 
 const createProduct = async (req, res) => {
+
   const product = await Product.create(req.body);
+
   res.status(201).json(product);
+
 };
+
+
 
 const updateProduct = async (req, res) => {
+
   const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
   if (!product) return res.status(404).json({ message: "Product not found" });
+
   return res.json(product);
+
 };
 
+
+
 const deleteProduct = async (req, res) => {
+
   const product = await Product.findByIdAndDelete(req.params.id);
+
   if (!product) return res.status(404).json({ message: "Product not found" });
+
   return res.json({ ok: true });
+
 };
+
+
 
 module.exports = { getProducts, getProductById, createProduct, updateProduct, deleteProduct };
